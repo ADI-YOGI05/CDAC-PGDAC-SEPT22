b6b6386209ada947642359a1cb434d17c820eba1
ADI-YOGI05 <113439383+ADI-YOGI05@users.noreply.github.com>
2022-09-13 23:49:29 +0530

    Add files via upload

---
+++ 13 sept/type 1/p18.java
@@ -0,0 +1,23 @@
+class pattern14{
+public static void main(String args[])
+{
+
+for( int i=4;i>=0;i--)
+{
+for ( int j=0;j<=i;j++)
+{
+System.out.print((char)(j+65)+" ");
+}
+//for( char j='A';j<=i;j++)
+//{
+//System.out.print((char)(j+65));
+
+//}
+
+    System.out.println();
+
+}
+
+}
+
+}
---
+++ 13 sept/type 3/starp14.java
@@ -0,0 +1,39 @@
+class starp14{
+public static void main(String args[])
+{
+
+for( int i=5;i>0;i--)
+{
+for ( int j=4;j>=i;j--)
+{
+System.out.print(" ");
+}
+for( int j=1;j<=i;j++)
+{
+System.out.print("* ");
+
+}
+
+    System.out.println();
+
+}
+
+for( int i=2;i<=5;i++)
+{
+for ( int j=4;j>=i;j--)
+{
+System.out.print(" ");
+}
+for( int j=1;j<=i;j++)
+{
+System.out.print("* ");
+
+}
+
+    System.out.println();
+
+}
+
+}
+
+}
