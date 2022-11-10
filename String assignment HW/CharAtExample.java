6457d059d621e1ef5fc907741628d8b546505d8e
ADI-YOGI05 <113439383+ADI-YOGI05@users.noreply.github.com>
2022-11-10 10:22:22 +0530

    Add files via upload

---
+++ String assignment HW/CharAtExample.java
@@ -0,0 +1,6 @@
+public class CharAtExample{  
+public static void main(String args[]){  
+String name="java";  
+char ch=name.charAt(3);//returns the char value at the 4th index  
+System.out.println(ch);  
+}}  
